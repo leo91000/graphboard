215cd3af8c9cd65abab2760670e3ea18e721e74a
Léo Coletta <mail.leo.coletta@gmail.com>
2021-12-22 12:58:49 +0100

    feat: Changed layout

--- client/src/components.d.ts
+++ client/src/components.d.ts
@@ -11,8 +11,8 @@ declare module 'vue' {
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
     CarbonPedestrian: typeof import('~icons/carbon/pedestrian')['default']
     CarbonSun: typeof import('~icons/carbon/sun')['default']
-    CarbonWarning: typeof import('~icons/carbon/warning')['default']
     Footer: typeof import('./components/Footer.vue')['default']
+    Header: typeof import('./components/Header.vue')['default']
     README: typeof import('./components/README.md')['default']
   }
 }
---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { isDark, toggleDark } from '~/composables'
+
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
+<template>
+  <div class="w-full px-4 py-3 bg-light-blue-400 dark:bg-light-blue-800 flex flex-row content-center justify-between">
+    <div>
+      {{ t('app.name') }}
+    </div>
+    <div class="flex flex-row items-center justify-center">
+      <button class="flex flex-row items-center justify-center icon-btn !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+        <carbon-moon v-if="isDark" />
+        <carbon-sun v-else />
+      </button>
+
+      <a class="flex flex-row items-center justify-center icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
+        <carbon-language />
+      </a>
+    </div>
+  </div>
+</template>
